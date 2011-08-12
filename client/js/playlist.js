f362f65cee9164ce2b7d01e46bcc275c13de4f62
alislee <alison.lee@nokia.com>
2011-08-11 19:46:24 -0700

    additional audio/video files

--- client/js/playlist.js
+++ client/js/playlist.js
@@ -1,13 +1,37 @@
 var playlist = [{
- 'url': 'http://hw.libsyn.com/p/3/f/2/3f2b87359f747ca9/cgpodcast069.mp3?sid=1174c931590c726cbbae59034d0893e1&l_sid=27094&l_eid=&l_mid=2466575',
- 'type': 'audio/mpeg',
- 'title': 'CoffeeGeek Podcast 069 - History of Espresso Part 1'
+ url: 'http://hw.libsyn.com/p/3/f/2/3f2b87359f747ca9/cgpodcast069.mp3?sid=1174c931590c726cbbae59034d0893e1&l_sid=27094&l_eid=&l_mid=2466575',
+ type: 'audio/mpeg',
+ title: 'CoffeeGeek Podcast 069 - History of Espresso Part 1'
 },{
- 'url': 'http://hw.libsyn.com/p/e/a/8/ea8cfa44e09eea02/cgpodcast068.mp3?sid=4540a5554c90eb34eb45e349311b9990&l_sid=27094&l_eid=&l_mid=2426351',
- 'type': 'audio/mpeg',
- 'title': 'CoffeeGeek Podcast 068 - 2010 in Review'
+ url: 'http://hw.libsyn.com/p/e/a/8/ea8cfa44e09eea02/cgpodcast068.mp3?sid=4540a5554c90eb34eb45e349311b9990&l_sid=27094&l_eid=&l_mid=2426351',
+ type: 'audio/mpeg',
+ title: 'CoffeeGeek Podcast 068 - 2010 in Review'
 },{
- 'url': 'http://hw.libsyn.com/p/2/e/1/2e193786efa3d59e/cgpodcast067.mp3?sid=7305e7da959212bee010b02d7873affe&l_sid=27094&l_eid=&l_mid=2364206',
- 'type': 'audio/mpeg',
- 'title': 'CoffeeGeek Podcast 067 - Vacpot History Part 2'
+ url: 'http://hw.libsyn.com/p/2/e/1/2e193786efa3d59e/cgpodcast067.mp3?sid=7305e7da959212bee010b02d7873affe&l_sid=27094&l_eid=&l_mid=2364206',
+ type: 'audio/mpeg',
+ title: 'CoffeeGeek Podcast 067 - Vacpot History Part 2'
+},{
+ url: 'http://hw.libsyn.com/p/d/4/0/d40135726e9e57b2/cgpodcast070.mp3?sid=5abe8509a01b099bd6e6f3df4c98a61e&l_sid=27094&l_eid=&l_mid=2485361',
+ type: 'audio/mpeg',
+ title: 'CoffeeGeek Podcast 070 - Christchurch Cafe'
+},{
+ url: 'http://hw.libsyn.com/p/e/7/5/e754c27f01eab861/cgpodcast066.mp3?sid=c3259ec95a92ae9d7719f392ed7dc147&l_sid=27094&l_eid=&l_mid=2293872',
+ type: 'audio/mpeg',
+ title: 'CoffeeGeek Podcast 066 - Vacpot History Part 1'
+},{
+ url: 'http://hw.libsyn.com/p/6/b/6/6b654d9c6ac68fd5/cgpodcast065.mp3?sid=4fb7d78c4957fe15e2ab6576035b8272&l_sid=27094&l_eid=&l_mid=2293873',
+ type: 'audio/mpeg',
+ title: 'CoffeeGeek Podcast 065 - Vario Grinder'
+},{
+ url: 'http://people.sc.fsu.edu/~jburkardt/data/mp4/cvt_cutoff_movie.mp4'
+ type: 'video/mp4',
+ title: 'The CVT Cutoff Movie'
+},{
+ url: 'http://www.gamefront.com/files/16994027/Quality_Sample_mp4',
+ type: 'video/mp4',
+ title: 'Quality Sample mp4 file'
+},{
+ url: 'http://www.808.dk/pics/video/gizmo.mp4',
+ type: 'video/mp4',
+ title: 'An HTML5 Video'
 }];
